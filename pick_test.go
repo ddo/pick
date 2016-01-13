d391cad45a978aa484de21897c5fe2ba97b322ea
ddo <joeddo89@gmail.com>
2016-01-13 16:56:47 +0700

    update testing

--- pick_test.go
+++ pick_test.go
@@ -103,15 +103,32 @@ func TestPickText(t *testing.T) {
 			"class",
 			"target",
 		},
-	})
+	}, 0)
 
 	if !reflect.DeepEqual(res, []string{"some text here", "some text here also"}) {
 		t.Fail()
 	}
 }
 
+func TestPickTextLimit(t *testing.T) {
+	pageSource := "<div>notme<p>should not include me</p>notme<p class='target'>some text here</p><p class='target'>some text here also</p>notme</div>"
+
+	res := PickText(&Option{
+		PageSource: strings.NewReader(pageSource),
+		TagName:    "p",
+		Attr: &Attr{
+			"class",
+			"target",
+		},
+	}, 1)
+
+	if !reflect.DeepEqual(res, []string{"some text here"}) {
+		t.Fail()
+	}
+}
+
 func TestPickTextTree(t *testing.T) {
-	pageSource := "<div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div>"
+	pageSource := "<div class='target'><script>console.log('haha')</script><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div>"
 
 	res := PickText(&Option{
 		PageSource: strings.NewReader(pageSource),
@@ -120,9 +137,70 @@ func TestPickTextTree(t *testing.T) {
 			"class",
 			"target",
 		},
-	})
+	}, 0)
 
-	if !reflect.DeepEqual(res, []string{"text1", "text2", "text3", "text4"}) {
+	if !reflect.DeepEqual(res, []string{"console.log('haha')text1text2text3text4"}) {
 		t.Fail()
+	}
+}
+
+func TestPickHtml(t *testing.T) {
+	pageSource := "<div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div><div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div>"
+
+	res := PickHtml(&Option{
+		PageSource: strings.NewReader(pageSource),
+		TagName:    "div",
+		Attr: &Attr{
+			"class",
+			"target",
+		},
+	}, 0)
+
+	if !reflect.DeepEqual(res, []string{"<div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div>", "<div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div>"}) {
+		t.Fail()
+	}
+}
+
+func TestPickHtmlLimit(t *testing.T) {
+	pageSource := "<div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div><div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div>"
+
+	res := PickHtml(&Option{
+		PageSource: strings.NewReader(pageSource),
+		TagName:    "div",
+		Attr: &Attr{
+			"class",
+			"target",
+		},
+	}, 1)
+
+	if !reflect.DeepEqual(res, []string{"<div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div>"}) {
+		t.Fail()
+	}
+}
+
+func TestPickHtmlNilAttr(t *testing.T) {
+	pageSource := "<div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div><div class='target'><div><p>text1</p>text2<ul><li>text3</li><li>text4</li></ul></div></div>"
+
+	res := PickHtml(&Option{
+		PageSource: strings.NewReader(pageSource),
+		TagName:    "ul",
+	}, 0)
+
+	if !reflect.DeepEqual(res, []string{"<li>text3</li><li>text4</li>", "<li>text3</li><li>text4</li>"}) {
+		t.Fail()
+	}
+}
+
+var selfClosingTag = [][]byte{
+	[]byte("input"),
+	[]byte("br"),
+	[]byte("hr"),
+}
+
+func TestIsSelfClosingTag(t *testing.T) {
+	for i := 0; i < len(selfClosingTag); i++ {
+		if !isSelfClosingTag(selfClosingTag[i]) {
+			t.Fail()
+		}
 	}
 }
